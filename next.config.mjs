aeb32e2d5cad0de9e3d67b722a1358b220af33bc
pritam-ago <pritam.developer.1@gmail.com>
2025-05-31 15:47:09 +0530

    modified next config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests: false"
+          }
+        ],
+      },
+    ]
+  }
 }
 
 export default nextConfig
