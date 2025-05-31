d2d7a92c2f74bb338cb6c0d172bcf19de46d0000
pritam-ago <pritam.developer.1@gmail.com>
2025-05-31 15:54:57 +0530

    modified next config

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "upgrade-insecure-requests"
+            value: "default-src 'self' http: https:; connect-src 'self' http: https:;"
           }
         ],
       },
