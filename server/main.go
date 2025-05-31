34da39d3964a382fd47358c95ec90c5d775fc0a4
pritam-ago <pritam.developer.1@gmail.com>
2025-05-31 17:20:19 +0530

    cors error

--- server/main.go
+++ server/main.go
@@ -26,7 +26,10 @@ func main() {
 
 	// CORS middleware
 	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins
+		origin := c.Request.Header.Get("Origin")
+		if origin == "https://wc1-todolist.vercel.app" || origin == "http://localhost:3000" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
